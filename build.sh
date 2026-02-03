6d768471d6520369a8ef1a2aa6d99906ed83b8f6
Kumkum Porwal <Kumkumporwal16@gmail.com>
2026-02-03 20:55:59 +0530

    Pin Python runtime to 3.11 and upgrade pip/wheel/setuptools in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Ensure pip, wheel, and setuptools are up-to-date to get binary wheels on the build image
+python -m pip install --upgrade pip wheel setuptools
+
 pip install -r requirements.txt
 
 cd myblog
